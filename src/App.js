1455f417459ab50d3b865a2799d899a44875f57b
Usama Hafeez <107476182+usamahafeez567@users.noreply.github.com>
2023-04-14 17:13:46 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Navbar from "./components/navbar";
-import "./App.css";
-import Signup from "./components/signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Signup from "./components/signup";
 import Contact from "./components/contact";
 import About from "./components/about";
 import Home from "./components/home";
 import Login from "./components/login";
 import Dashboard from "./components/dashboard";
+import Services from "./components/services";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Routes>
               <Route exact path="/" Component={Home}></Route>
               <Route path="/about" Component={About}></Route>
+              <Route path="/services" Component={Services}></Route>
               <Route path="/contact" Component={Contact}></Route>
               <Route path="/signup" Component={Signup}></Route>
               <Route path="/login" Component={Login}></Route>
